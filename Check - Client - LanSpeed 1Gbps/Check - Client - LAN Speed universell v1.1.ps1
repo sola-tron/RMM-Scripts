1be27d99bb392146be7e2a6844d1def73b964ba8
sola-tron <95682998+sola-tron@users.noreply.github.com>
2022-12-15 15:17:45 +0100

    Add files via upload

---
+++ Check - Client - LanSpeed 1Gbps/Check - Client - LAN Speed universell v1.1.ps1
@@ -0,0 +1,21 @@
+﻿$adapters = Get-NetAdapter -Physical | ? Status -eq "Up" # nur die aktiven Netzwerkkarten auslesen
+$errors = $false
+
+foreach ($adapter in $adapters) {
+
+    $speed = $adapter.LinkSpeed
+
+    if ($speed -eq "1 Gbps") {
+        Write-Host "LAN Geschwindigkeit:" $speed
+    } else {
+        Write-Host "FEHLER! LAN Geschwindigkeit: $speed ($($adapter.InterfaceAlias) / $($adapter.InterfaceDescription))"
+        $errors = $true
+    }
+
+}
+
+if ($errors) {
+    exit 1001
+} else {
+    exit 0
+}
